46494bbe00ecfb6d39ce9744d12611ecda816b0e
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-04-27 14:42:04 +0200

    Create positions table

--- Server/pizza.sql
+++ Server/pizza.sql
@@ -1,104 +1,144 @@
--- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: pizza
--- ------------------------------------------------------
--- Server version	10.4.24-MariaDB
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2022. Ápr 27. 14:39
+-- Kiszolgáló verziója: 10.4.22-MariaDB
+-- PHP verzió: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `orders`
+-- Adatbázis: `pizza`
 --
 
-DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `orders`
+--
+
 CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
   `pizza_id` int(11) DEFAULT NULL,
-  `topping_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `FK_foreing` (`topping_id`),
-  KEY `FK_pizza` (`pizza_id`),
-  CONSTRAINT `FK_foreing` FOREIGN KEY (`topping_id`) REFERENCES `toppings` (`toppings_id`),
-  CONSTRAINT `FK_pizza` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`number`)
+  `topping_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `orders`
+-- Tábla szerkezet ehhez a táblához `pizza`
 --
 
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `pizza`
---
-
-DROP TABLE IF EXISTS `pizza`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pizza` (
-  `number` int(11) NOT NULL AUTO_INCREMENT,
+  `number` int(11) NOT NULL,
   `name` varchar(40) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
-  `diameter` int(11) DEFAULT NULL,
-  PRIMARY KEY (`number`)
+  `diameter` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `pizza`
+-- Tábla szerkezet ehhez a táblához `positions`
 --
 
-LOCK TABLES `pizza` WRITE;
-/*!40000 ALTER TABLE `pizza` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pizza` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `positions` (
+  `position_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `toppings`
+-- Tábla szerkezet ehhez a táblához `toppings`
 --
 
-DROP TABLE IF EXISTS `toppings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `toppings` (
-  `toppings_id` int(11) NOT NULL AUTO_INCREMENT,
+  `toppings_id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
-  PRIMARY KEY (`toppings_id`)
+  `price` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `toppings`
+-- Indexek a kiírt táblákhoz
 --
 
-LOCK TABLES `toppings` WRITE;
-/*!40000 ALTER TABLE `toppings` DISABLE KEYS */;
-/*!40000 ALTER TABLE `toppings` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- A tábla indexei `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `FK_foreing` (`topping_id`),
+  ADD KEY `FK_pizza` (`pizza_id`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- A tábla indexei `pizza`
+--
+ALTER TABLE `pizza`
+  ADD PRIMARY KEY (`number`);
+
+--
+-- A tábla indexei `positions`
+--
+ALTER TABLE `positions`
+  ADD PRIMARY KEY (`position_id`);
+
+--
+-- A tábla indexei `toppings`
+--
+ALTER TABLE `toppings`
+  ADD PRIMARY KEY (`toppings_id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `pizza`
+--
+ALTER TABLE `pizza`
+  MODIFY `number` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `positions`
+--
+ALTER TABLE `positions`
+  MODIFY `position_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `toppings`
+--
+ALTER TABLE `toppings`
+  MODIFY `toppings_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `FK_foreing` FOREIGN KEY (`topping_id`) REFERENCES `toppings` (`toppings_id`),
+  ADD CONSTRAINT `FK_pizza` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`number`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-04-21 17:57:08
