6c4093810bcba93a5be5ac6a2f73352ec382d8c6
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2022-04-27 14:49:19 +0200

    Create users table

--- Server/pizza.sql
+++ Server/pizza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2022. Ápr 27. 14:39
+-- Létrehozás ideje: 2022. Ápr 27. 14:48
 -- Kiszolgáló verziója: 10.4.22-MariaDB
 -- PHP verzió: 8.1.2
 
@@ -69,6 +69,18 @@ CREATE TABLE `toppings` (
   `price` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `position_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexek a kiírt táblákhoz
 --
@@ -98,6 +110,13 @@ ALTER TABLE `positions`
 --
 ALTER TABLE `toppings`
   ADD PRIMARY KEY (`toppings_id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`),
+  ADD KEY `FK_position` (`position_id`);
 
 --
 -- A kiírt táblák AUTO_INCREMENT értéke
@@ -137,6 +156,12 @@ ALTER TABLE `toppings`
 ALTER TABLE `orders`
   ADD CONSTRAINT `FK_foreing` FOREIGN KEY (`topping_id`) REFERENCES `toppings` (`toppings_id`),
   ADD CONSTRAINT `FK_pizza` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`number`);
+
+--
+-- Megkötések a táblához `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `FK_position` FOREIGN KEY (`position_id`) REFERENCES `positions` (`position_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
