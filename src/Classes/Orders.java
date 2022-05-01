60ad14f72a58ed54218ed6d53eb7816bb4b85fa5
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-05-01 12:20:59 +0200

    Create Entities

---
+++ src/Classes/Orders.java
@@ -0,0 +1,56 @@
+package Classes;
+
+import java.util.List;
+
+public class Orders {
+
+    public Orders(int order_id, int pizza_id, int topping_id, int delivery_id) {
+        this.setOrder_id(order_id);
+        this.setPizza_id(pizza_id);
+        this.setTopping_id(topping_id);
+        this.setDelivery_id(delivery_id);
+    }
+
+
+    int order_id;
+
+    int pizza_id;
+
+    int topping_id;
+
+    int delivery_id;
+
+    public int getTopping_id() {
+        return topping_id;
+    }
+
+    public void setTopping_id(int topping_id) {
+        this.topping_id = topping_id;
+    }
+
+    public int getDelivery_id() {
+        return delivery_id;
+    }
+
+    public void setDelivery_id(int delivery_id) {
+        this.delivery_id = delivery_id;
+    }
+
+    public int getPizza_id() {
+        return pizza_id;
+    }
+
+    public void setPizza_id(int pizza_id) {
+        this.pizza_id = pizza_id;
+    }
+
+    public int getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
+    }
+
+    List<Orders> ordersList;
+}
---
+++ src/Classes/Users.java
@@ -0,0 +1,44 @@
+package Classes;
+
+import java.util.List;
+
+public class Users {
+
+    public Users(String Username, String Password, int Position_id){
+        this.setUsername(Username);
+        this.setPassword(Password);
+        this.setPosition_id(Position_id);
+    }
+
+    private String username;
+
+    private String password;
+
+    private int position_id;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPosition_id() {
+        return position_id;
+    }
+
+    public void setPosition_id(int position_id) {
+        this.position_id = position_id;
+    }
+
+    private List<Users> usersList;
+}
