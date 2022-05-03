ea217312ce0add3db77eb116061943b81eca6e69
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2022-05-03 15:50:55 +0200

    Register form created

---
+++ src/main/java/Windows/Register.java
@@ -0,0 +1,34 @@
+package Windows;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Register extends JDialog{
+
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton registerButton;
+    private JButton cancelButton;
+    private JPanel Register;
+
+    public Register(){
+        setTitle("Registration");
+        setContentPane(this.Register);
+        setMinimumSize(new Dimension(800,500));
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+    }
+}
