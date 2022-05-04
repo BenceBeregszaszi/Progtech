7b14d0bf52f233bf335acf2f00de1e90a6ba01f0
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2022-05-04 14:50:13 +0200

    Pizzas form method

--- src/main/java/Windows/Pizzas.java
+++ src/main/java/Windows/Pizzas.java
@@ -1,11 +1,30 @@
 package Windows;
 
-import javax.swing.*;
+import Classes.Pizza;
 
-public class Pizzas {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Pizzas extends JDialog {
     private JList list1;
     private JButton btn_confirm;
     private JTextField tb_name;
     private JTextField tb_price;
     private JTextField tb_diameter;
+    
+    public Pizzas() {
+        setTitle("Pizzas");
+        setContentPane(pizzas_panel);
+        setMinimumSize(new Dimension(1024, 800));
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+        btn_confirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Pizza.AddPizza(tb_name.getText(), Integer.parseInt(tb_price.getText()), Integer.parseInt(tb_diameter.getText()));
+            }
+        });
+    }
 }
