584e5991beb25b7ff16389086201cd5a94c28f62
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2022-05-04 14:12:16 +0200

    Create Salami Decorator

---
+++ src/main/java/Classes/Decorators/Salami.java
@@ -0,0 +1,30 @@
+
+   
+package Classes.Decorators;
+
+import Abstract.Decorator;
+import Classes.Pizza;
+
+public class Salami  extends Decorator {
+
+    Pizza pizza;
+
+    public Salami(Pizza pizza){
+        this.pizza=pizza;
+    }
+
+    @Override
+    protected String getname() {
+        return pizza.getName() + "Szalámival";
+    }
+
+    @Override
+    protected int getdiameter() {
+        return pizza.getDiameter();
+    }
+
+    @Override
+    protected int getcost() {
+        return pizza.getPrice() + decorator_price;
+    }
+}
