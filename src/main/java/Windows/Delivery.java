764e8cdd7916d36684a61d5d3b9e69567764cd28
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2022-05-04 15:05:43 +0200

    Delivery Button

--- src/main/java/Windows/Delivery.java
+++ src/main/java/Windows/Delivery.java
@@ -1,10 +1,29 @@
 package Windows;
 
+import Classes.Deliveries;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Delivery {
     private JList list1;
     private JButton btn_confirm;
     private JTextField tb_location;
     private JTextField tb_name;
+    
+    public Delivery() {
+        setTitle("Delivery");
+        setContentPane(delivery_panel);
+        setMinimumSize(new Dimension(1024, 800));
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+        btn_confirm.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Deliveries.Create(tb_location.getText(), tb_name.getText());
+            }
+        });
+    }
 }
