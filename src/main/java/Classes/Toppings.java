402ce62b9d1ca8075feb22f0ef4a81b25e57c3a9
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-05-05 16:50:20 +0200

    List Toppings

--- src/main/java/Classes/Toppings.java
+++ src/main/java/Classes/Toppings.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,11 +21,35 @@ public class Toppings {
 
     int price;
 
+    public static Toppings[] getToppings(){
+        Toppings[] temp = null;
+        try {
+            List<Toppings> helper = new ArrayList<>();
+            Connection conn = DataNode.getConnection();
+            String query = "SELECT * FROM toppings";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                Toppings actual = new Toppings(rs.getInt("toppings_id"), rs.getString("name"), rs.getInt("price"));
+                helper.add(actual);
+            }
+            conn.close();
+            temp = new Toppings[helper.size()];
+            helper.toArray(temp);
+        } catch (SQLException e) {
+            //log
+        }
+        catch (Exception ex){
+            //log
+        }
+        return temp;
+    }
+
 
     public static void Create(String name, int price){
         try {
             Connection conn = DataNode.getConnection();
-            String query = "INSERT INTO toppings VALUES (?, ?)";
+            String query = "INSERT INTO toppings(name, price) VALUES (?, ?)";
             PreparedStatement st = conn.prepareStatement(query);
             st.setString(1,name);
             st.setInt(2,price);
@@ -33,9 +59,11 @@ public class Toppings {
         } catch (SQLException e) {
             //log
         }
+        //log
     }
 
-    public void Delete(){
-        //log
+    @Override
+    public String toString() {
+        return name;
     }
 }
