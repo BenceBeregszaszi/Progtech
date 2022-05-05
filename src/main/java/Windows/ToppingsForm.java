cf1aece83a56b23e33aa04c97524a2ae03203612
Bence Beregszászi <91261932+BenceBeregszaszi@users.noreply.github.com>
2022-05-05 16:49:41 +0200

    Rename Toppings form

--- src/main/java/Windows/ToppingsForm.java
+++ src/main/java/Windows/ToppingsForm.java
@@ -1,18 +1,20 @@
 package Windows;
 
+import Classes.Toppings;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Toppings extends JDialog{
+public class ToppingsForm extends JDialog{
     private JList list1;
     private JTextField tb_price;
     private JTextField tb_name;
     private JButton btn_confirm;
     private JPanel toppings_panel;
 
-    public Toppings() {
+    public ToppingsForm() {
         setTitle("Toppings");
         setContentPane(toppings_panel);
         setMinimumSize(new Dimension(1024, 800));
@@ -21,7 +23,7 @@ public class Toppings extends JDialog{
         btn_confirm.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                Toppings.Create(tb_name.getText(), Integer.parseInt(tb_price.getText()));
             }
         });
     }
