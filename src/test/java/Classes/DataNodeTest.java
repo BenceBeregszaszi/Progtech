bf4e612c2e122633d245d1babfe10ac9a17229cb
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2022-05-19 20:49:16 +0200

    Unit Test - DataNode Test

--- src/test/java/Classes/DataNodeTest.java
+++ src/test/java/Classes/DataNodeTest.java
@@ -2,11 +2,27 @@ package Classes;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class DataNodeTest {
 
     @Test
     void getConnection() {
-
+        Connection expected = null;
+        Connection actual = null;
+        try {
+            String jdbcURL = "jdbc:mysql://localhost:3306/pizza";
+            String username = "ROOT";
+            String password = "ROOT";
+            expected  =  DriverManager.getConnection(jdbcURL, username, password);
+            actual = DataNode.getConnection();
+            }
+        catch (Exception e) {
+            LoggerClass.ExceptionLog(e.getMessage());
+        }
+   assertEquals(expected.getClass(), actual.getClass());
     }
 }
