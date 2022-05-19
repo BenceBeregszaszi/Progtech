2363bd6034276db74d802dffc7a72628fa071360
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2022-05-19 20:53:20 +0200

    Unit Test - DeliveriesTest

--- src/test/java/Classes/DeliveriesTest.java
+++ src/test/java/Classes/DeliveriesTest.java
@@ -2,23 +2,58 @@ package Classes;
 
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 class DeliveriesTest {
 
     @Test
     void read() {
+        List<Deliveries> expected = new ArrayList<>();
+        List<Deliveries> actual = new ArrayList<>();
+        try {
+            Connection conn = DataNode.getConnection();
+            String query = "SELECT * FROM delivery";
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                Deliveries temp = new Deliveries(rs.getInt("delivery_id"), rs.getString("location"), rs.getString("name"));
+                expected.add(temp);
+            }
+        }catch (Exception e) {
+            LoggerClass.ExceptionLog(e.getMessage());
+        }
+        Deliveries[] helper = Deliveries.Read();
+        for (int i = 0; i < helper.length; i++) {
+            actual.add(helper[i]);
+        }
+        for (int i = 0; i < actual.size(); i++) {
+            assertEquals(actual.get(i).getName(), expected.get(i).getName());
+        }
 
     }
 
     @Test
     void setDelivery_id() {
+        Deliveries actual = new Deliveries(1,"Eger", "Teletál");
+        int expected = 1;
+        assertEquals(expected, actual.delivery_id);
     }
 
     @Test
     void setLocation() {
+        Deliveries actual = new Deliveries(1,"Eger", "Teletál");
+        String expected = "Eger";
+        assertEquals(expected, actual.location);
     }
 
     @Test
     void setName() {
+        Deliveries actual = new Deliveries(1,"Eger", "Teletál");
+        String expected = "Teletál";
+        assertEquals(expected, actual.name);
     }
 }
